333df2c8b2655b92f19884bc82537e0f541f2d25
dimaska <parshindi@mail.ru>
2023-01-21 19:46:37 +0200

    Added test for menu

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,27 +1,10 @@
 import pytest
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from starlette.testclient import TestClient
 
-from config import DATABASE_URL
-from db.models import Base
-
-test_engine = create_engine(DATABASE_URL)
-
-test_session = sessionmaker(
-    autocommit=False, autoflush=False, bind=test_engine
-)
+from main import app
 
 
-@pytest.fixture(scope='session', autouse=True)
-def create_tables():
-    Base.metadata.create_all(test_engine)
-
-
-@pytest.fixture(scope='session', autouse=True)
-def drop_tables():
-    Base.metadata.drop_all(test_engine)
-
-
-
-
-
+@pytest.fixture(scope="module")
+def test_app():
+    client = TestClient(app)
+    yield client
